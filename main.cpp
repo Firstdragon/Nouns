9d0203ac8df11cf8145f271fd11fb9f5f1e57faa
Firstdragon <anden777@ya.ru>
2014-12-25 00:35:58 +0300

    New

--- main.cpp
+++ main.cpp
@@ -1,31 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <locale.h>
-#include "Irregular.h"
-#include "Regular.h"
+#include "Nouns.h"
+#include "rus8.h"
 using namespace std;
 
-void filein(nouns *noun, int &n){
-	nouns nauns;
-	string str;
-	int choice = 0;
-	ifstream in("in.txt");
-	if (in.is_open())
-		while (in.good()){
-			getline(in, str);
-			int k = nauns.obrabotka(noun, n, str, choice);
-			if (k!=0)
-			ifregular(str, choice);
-		}
-}
-
 int main(){
-	nouns nauns;
-	setlocale(LC_ALL, "Russian");
+	vhod vohd;
+	rus();
 	int n;
-	nouns *noun = nauns.read(n);
-	filein(noun, n);
+	vohd.menu();
 	system("pause");
 	return 0;
 }
