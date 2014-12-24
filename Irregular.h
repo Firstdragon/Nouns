2a9a0bf22dd7e8f35c8a799f047d6789bcfd2332
Firstdragon <anden777@ya.ru>
2014-12-25 01:10:45 +0300

    New

--- Irregular.h
+++ Irregular.h
@@ -1,8 +1,27 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <fstream>
+#include "rus8.h"
+#include "Nouns.h"
+
+using namespace std;
+
+
 class irregulars
 {
-public:
+private:
 	string singular;
 	string plural;
+
+public:
+
+	irregulars()
+	{
+		string singular = "";
+		string plural = "";
+	}
+
 
 	irregulars* read(int &n)
 	{
@@ -31,6 +50,27 @@ public:
 			return noun;
 		}
 	}
+
+	int checkirregular(irregulars *noun, int &n, string &str, int &choice)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			if (noun[i].singular == str)
+			{
+				if (choice == 1) cout << "Слово в единственном числе" << endl;
+				else cout << noun[i].plural << endl;
+				return 0;
+			}
+			else if (noun[i].plural == str)
+			{
+				if (choice == 1) cout << "Слово во множественном числе" << endl;
+				else cout << noun[i].singular << endl;
+				return 0;
+			}
+		}
+		return 1;
+	}
+
 
 	~irregulars()
 	{
